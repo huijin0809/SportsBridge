60275300b7ddad194e0120f9188ee8468e2d4053
huijin0809 <huijin0809@naver.com>
2024-02-08 18:26:03 +0900

    프로젝트 생성 및 초기 설정 파일 작성 (application.properties)

---
+++ learnNest/src/main/java/com/me/learnNest/LearnNestApplication.java
@@ -0,0 +1,13 @@
+package com.me.learnNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnNestApplication.class, args);
+	}
+
+}
